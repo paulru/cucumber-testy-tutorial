48f1188865f36799e9ef5c0b4fc78a064c4df270
Andrei C <paulie.rusu@gmail.com>
2015-07-03 14:50:24 +0300

    our first cucumber steps and commit

--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -1,6 +1,12 @@
 package org.fasttrackit.workshop.login;
 
+import com.sun.javafx.geom.AreaOp;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import com.sun.xml.internal.messaging.saaj.util.SAAJUtil;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.fasttrackit.util.TestBaseNative;
 import org.fasttrackit.workshop.pagefactory.login.LoginPage;
@@ -27,4 +33,47 @@ public class LoginSteps extends TestBaseNative {
         element.sendKeys(email);
     }
 
+    @Given("^i access the login page$")
+    public void i_access_the_login_page()  {
+        
+    }
+
+    @And("^i insert valid credential$")
+    public void i_insert_valid_credential()  {
+    }
+
+    @When("^i click login button$")
+    public void i_click_login_button()  {
+    }
+
+    @Then("^i check if user is logged in$")
+    public void i_check_if_user_is_logged_in()  {
+        // Express the Regexp above with the code you wish you had
+        
+    }
+
+    @And("^i insert invalid credential$")
+    public void i_insert_invalid_credential()  {
+        // Express the Regexp above with the code you wish you had
+        
+    }
+
+    @Then("^i expect invalid credential message$")
+    public void i_expect_invalid_credential_message()  {
+        // Express the Regexp above with the code you wish you had
+        
+    }
+
+
+
+
+    @Given("^i open this url \"([^\"]*)\"$")
+    public void i_open_this_url(String url) throws Throwable {
+     driver.get(url);
+    }
+
+    @Then("^i send (\\d+) into search field$")
+    public void i_send_into_search_field(int arg1) throws Throwable {
+        System.out.println("numarul este " + arg1);
+    }
 }
--- src/test/resources/feature/workshop/login.feature
+++ src/test/resources/feature/workshop/login.feature
@@ -1,1 +1,26 @@
+Feature: login
+
+  Scenario: Open any page
+    Given i open this url "http://google.com"
+    Then i send 10 into search field
+
+
+  Scenario: Login successfullyuuuu
+    Given i access the login page
+    And i insert valid credential
+    When i click login button
+    Then i check if user is logged in
+
+
+#  Scenario: Log in with wrong credentials
+#    Given i access the login page
+#    And i insert invalid credential
+#    When i click login button
+#    Then i expect invalid credential message
+
+
+
+
+
+
 
